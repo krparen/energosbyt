f8fc870253107285f6c08b609bfed997ec514bab
g.eremeev <g.eremeev@azoft.com>
2020-09-01 09:21:33 +0300

    rabbit properties moved to application.yml

--- src/main/java/com/azoft/energosbyt/config/RabbitConfig.java
+++ src/main/java/com/azoft/energosbyt/config/RabbitConfig.java
@@ -4,16 +4,33 @@ import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
 
+  @Value("${energosbyt.rabbit.host}")
+  private String rabbitHost;
+  @Value("${energosbyt.rabbit.port}")
+  private int rabbitPort;
+  @Value("${energosbyt.rabbit.username}")
+  private String rabbitUser;
+  @Value("${energosbyt.rabbit.password}")
+  private String rabbitPassword;
+
+
+
   @Bean
   public ConnectionFactory connectionFactory() {
     CachingConnectionFactory connectionFactory =
-        new CachingConnectionFactory("localhost");
+        new CachingConnectionFactory();
+    connectionFactory.setHost(rabbitHost);
+    connectionFactory.setPort(rabbitPort);
+    connectionFactory.setUsername(rabbitUser);
+    connectionFactory.setPassword(rabbitPassword);
+
     return connectionFactory;
   }
 
